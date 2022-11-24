cb24e7798043a61e2b5c3936259feeb7ae045fd5
prathiksha1909 <87687761+prathiksha1909@users.noreply.github.com>
2022-11-24 08:09:02 +0530

    Add files via upload

---
+++ springboot-helloworld-tutorial/src/main/java/net/javaguides/springboot/springboothelloworldtutorial/User.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.springboothelloworldtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class User {
+
+	public static void main(String[] args) {
+		SpringApplication.run(User.class, args);
+	}
+
+}
